c3f9c06b6f8eb90b2c7598412f6efad2f71946c2
Samuel Matthew <samuelmatthew071@gmail.com>
2025-12-23 09:25:09 +0100

    Add temporary JSON rendering for exceptions in application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,28 +10,16 @@ use App\Http\Middleware\IsAdmin;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-
-
+    ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'IsAdmin' => IsAdmin::class,
         ]);
-        //
-        // Add middleware to the web group (applied to all web routes)
-        // $middleware->appendToGroup('web', [
-        //     AuthRedirect::class,   // protects authenticated routes
-        //     GuestRedirect::class,  // protects guest-only routes
-        // ]);
-
-        // // Define aliases for route-level use
-        // $middleware->alias('auth', AuthRedirect::class);       // use ->middleware('auth')
-        // $middleware->alias('guest', GuestRedirect::class);    // use ->middleware('guest')
-        // $middleware->alias('is_admin', IsAdmin::class);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        // ADD THIS LINE TEMPORARILY
+        $exceptions->shouldRenderJsonWhen(fn() => true);
     })->create();
